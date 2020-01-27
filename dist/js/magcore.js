76385eb3f0b52a515f3b1a5c21c104d5951ead88
Zebadiah K. Steeby <zsteeby@gmail.com>
2020-01-26 22:16:44 -0600

    wire up layer list and toggle

--- dist/js/magcore.js
+++ dist/js/magcore.js
@@ -894,7 +894,7 @@ define('magcore/utils/application',[], function () {
   return appUtils;
 });
 
-define('magcore/widgets/templates/layer-list.html',[],function () { return '<div>\r\n  <span class="layersTitle">TEST: Select any of the items from\r\n    the list below to add to the map.</span>\r\n  <div dojoattachpoint="layerList"></div>\r\n</div>';});
+define('magcore/widgets/templates/layer-list.html',[],function () { return '<div>\r\n  <span class="layersTitle">Select any of the items from\r\n    the list below to add to the map.</span>\r\n  <div dojoattachpoint="layerList"></div>\r\n</div>';});
 
 define('magcore/widgets/layer-list',[
   "./templates/layer-list.html",
@@ -922,11 +922,83 @@ define('magcore/widgets/layer-list',[
        * @param {Object[]} options.layers - An array of individual layer options.
        * @param {external:Map} options.map - A map instance containing the layers.
        */
+
+      _map: {},
       constructor: function ({ map, layers }, domNode) {
-        
+
+      this.layers = layers;
+      _map = map;
+
       },
       postCreate: function () {
-        $(this.layerList).html("TEST");
+
+        var arr = this.layers.sort((a, b) => a.layerListOrder - b.layerListOrder);
+        
+        for (var i = 0; i < arr.length; i++) {
+            var conf = arr[i];
+
+            if (conf.id !== "blockGroups") {
+                $(this.layerList).append(this.getCheckBoxHTML(conf));
+            }
+        }
+        $(this.layerList).find(".checkbox-div").click(this.toggleLayerItem);
+        $(this.layerList).find("[data-toggle=popover]").popover();
+
+        //$(this.layerList).html("TEST");
+      },
+      toggleLayerItem: function(e) {
+
+        //Toggle Checkbox
+        let $cbox = $(this).find(".big-checkbox");
+        $cbox.prop("checked", !$cbox.prop("checked"));
+
+        let layerId = $cbox.data("layer-id");
+
+        //Toggle Layer
+        let layer = _map.findLayerById(layerId);
+        if (layer) {
+            layer.visible = !layer.visible;
+        } else {
+            let grpLayers = getLayersByGroupId(layerId);
+
+            for (var i = 0; i < grpLayers.length; i++) {
+                var grpLayer = grpLayers[i];
+
+                let lay = _map.findLayerById(grpLayer.id);
+                if (lay) {
+                    lay.visible = !lay.visible;
+                }
+            }
+        }
+      },
+      getCheckBoxHTML: function(conf) {
+        var c = $.extend({}, conf);
+
+        if (c.legend.group) {
+            c.id = c.legend.group.id;
+            c.title = c.legend.group.title;
+        }
+
+        return `
+                <div>
+                    <div class="checkbox-div">
+                        <input type="checkbox" dojoattachpoint="c-${c.id}" ${c.visible ? "checked" : ""} data-layer-id="${c.id}" class="regular-checkbox big-checkbox" />
+                        <label></label>
+                        <label class="layerLabel">${c.title}</label>
+                        <a
+                        style="height: 25px;"
+                        tabindex="0"
+                        role="button"
+                        data-html="true"
+                        data-toggle="popover"
+                        data-placement="auto"
+                        data-trigger="hover"
+                        title="${c.title}"
+                        data-content="${c.definition}"><i class="fas fa-info-circle"></i>
+                        </a>
+                    </div>
+                </div>
+                `;
       },
       declaredClass: 'mag-layer-list'
     });
